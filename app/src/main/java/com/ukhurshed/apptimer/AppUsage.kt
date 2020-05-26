fc172f0ede38aedf9cc02e5ce54a541e2db375d6
JulianaOsi <julia.osintseva15@gmail.com>
2020-05-26 14:41:12 +0500

    getting usages fixed, icon added

--- app/src/main/java/com/ukhurshed/apptimer/AppUsage.kt
+++ app/src/main/java/com/ukhurshed/apptimer/AppUsage.kt
@@ -7,27 +7,31 @@ import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.annotation.RequiresApi
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.util.*
 import kotlin.math.abs
 
-@RequiresApi(Build.VERSION_CODES.Q)
+@RequiresApi(Build.VERSION_CODES.O)
 fun getAppUsages(context: Context, interval: Int): List<String> {
     val manager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
 
+    val starOfDay = getStartOfDay()
     val currentTime = System.currentTimeMillis()
-    val delta = 60 * 60 * 1000 * 24 *
-            when (interval) {
-                UsageStatsManager.INTERVAL_DAILY -> 1
-                UsageStatsManager.INTERVAL_WEEKLY -> 7
-                UsageStatsManager.INTERVAL_MONTHLY -> 7 * 31
-                else -> throw IllegalArgumentException()
-            }
-    val usageStatsList = manager.queryUsageStats(interval, currentTime - delta, currentTime)
+
+    val usageStatsList = manager.queryUsageStats(interval, starOfDay, currentTime)
 
     return usageStatsList
         .map { stat -> stat.packageName }
         .mapNotNull { name -> getAppInfo(name, context) }
         .mapNotNull { info -> getAppLabel(info, context) }
-        .mapIndexedNotNull { index, label ->  getTime(index, label, usageStatsList) }
+        .mapIndexedNotNull { index, label -> getTime(index, label, usageStatsList) }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun getStartOfDay(): Long {
+    val date = Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+    return date.atStartOfDay().toEpochSecond(ZoneOffset.UTC) * 1000
 }
 
 fun getAppInfo(name: String, context: Context): ApplicationInfo? {
@@ -46,8 +50,8 @@ fun getAppLabel(info: ApplicationInfo, context: Context): String? {
     }
 }
 
-fun getTime(index: Int, label: String, list: List<UsageStats>) : String? {
+fun getTime(index: Int, label: String, list: List<UsageStats>): String? {
     val time = list[index].totalTimeInForeground / 1000.0 / 60 / 60
-    return if (abs(time - 0.0) < 0.1)  null else
-        "$label ${String.format("%.1f", time)}"
+    return if (abs(time - 0.0) < 0.1) null else
+        "$label ${String.format("%.1f h", time)}"
 }
--- app/src/main/java/com/ukhurshed/apptimer/MainActivity.kt
+++ app/src/main/java/com/ukhurshed/apptimer/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.ukhurshed.apptimer
 
 import android.annotation.SuppressLint
+import android.app.AppOpsManager
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Process
 import android.provider.Settings
-import android.view.View
-import android.widget.*
-import org.w3c.dom.Text
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Spinner
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
-        val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
-        startActivity(intent)
+
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(
+            AppOpsManager.OPSTR_GET_USAGE_STATS,
+            Process.myUid(), packageName
+        )
+        if (mode != AppOpsManager.MODE_ALLOWED){
+            val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+            startActivity(intent)
+        }
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
